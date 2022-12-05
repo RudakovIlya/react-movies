dbc4474410796756d7b995e7520083e051b03c7a
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-05 22:04:34 +0300

    test commit

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -13,7 +13,7 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, isLoading: false}))
             .catch((err) => {
@@ -24,7 +24,7 @@ class Main extends Component {
 
     searchMovies = (str, type) => {
         this.setState({loading: true});
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, isLoading: false}))
             .catch((err) => {
